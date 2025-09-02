053a75b907ff5ac4cedaa9fd097d5996679177e3
Abhinavtej Reddy <87389936+Abhinavtej@users.noreply.github.com>
2025-09-02 17:02:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,22 +1,8 @@
-import java.util.logging.Logger;
-
 /**
  * A simple program that prints Hello, World!
  */
-public final class HelloWorld {
-
-    // Logger instance
-    private static final Logger LOGGER = Logger.getLogger(HelloWorld.class.getName());
-
-    // Private constructor to prevent instantiation
-    private HelloWorld() {}
-
-    /**
-     * Entry point of the program.
-     * @param args Command line arguments (unused)
-     */
+public class HelloWorld {
     public static void main(String[] args) {
-        // Using logger instead of System.out
-        LOGGER.info("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
