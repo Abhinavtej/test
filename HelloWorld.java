4ae2c9610afaad24fb9eb245a98caf0c815720da
Abhinavtej Reddy <87389936+Abhinavtej@users.noreply.github.com>
2025-09-02 16:58:24 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * A simple program that prints Hello, World!
+ */
+public class HelloWorld {
+
+    /**
+     * Prints a greeting message.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
